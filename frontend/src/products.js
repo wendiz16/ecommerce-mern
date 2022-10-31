716226878df8490027a675b202f3dcb25034db4b
Wendi Zhang <wendizhang68@gmail.com>
2022-10-31 14:34:21 -0400

    updated front end code

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Casual Handbag with Big Capacity',
+    image: '/imgs/bag-white.jpg',
+    description:
+      'This handbag is made of high quality cotton, lightweight, comfortable and durable. Makes you look not only fashionable, but also very casual.You can match any clothes, comfortable to carry and never go out of style.',
+    brand: 'unknown',
+    category: 'Fashion',
+    price: 19.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Backpack Purse Nylon',
+    image: '/imgs/bagpack-blue.jpg',
+    description:
+      'This backpack is made of high-quality Nylon with anti-theft design, water-resistant and lightweight, durable for everyday use, can keep your belongings safe.',
+    brand: 'BCD',
+    category: 'Fashion',
+    price: 29.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Recycled Cotton Utility Backpack',
+    image: '/imgs/bagpack-white.jpg',
+    description:
+      'Has a sturdy and comfortable shoulder strap. No need to worry about quality.It can be used as a hiking backpack, causal travel daypack, high school or college bag, shopping daypack, work or office rucksack etc.',
+    brand: 'ACF',
+    category: 'Fashion',
+    price: 25.49,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Handbag Flap Envelope',
+    image: '/imgs/handbag-red.jpg',
+    description:
+      'Classic flap envelope design, full of soft and lush touching feel, easy to match your shoes, dress,or nail polish, add a subtle glow to your evening formal attire and suede shoes.',
+    brand: 'HMM',
+    category: 'Fashion',
+    price: 99.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Handbag with Phone Pocket',
+    image: '/imgs/handbag-yellow.jpg',
+    description:
+      'Classic flap envelope design with metal binding trim',
+    brand: 'CKK',
+    category: 'Fashion',
+    price: 149.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Compact Sling Bag',
+    image: '/imgs/slingbag-black.jpg',
+    description:
+      'This waist bag has unique designer, it looks simple and smart , not out of time.',
+    brand: 'Guccci',
+    category: 'Fashion',
+    price: 429.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
