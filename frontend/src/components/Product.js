ef9c8280ae0977a4eaf6eb4fefa24e57b745b5e2
Wendi Zhang <wendizhang68@gmail.com>
2022-11-14 15:51:12 -0500

    updated front end and added new packages

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -9,19 +9,19 @@ const Product = ({product}) => {
         <Card.Img src={product.image} variant='top' />
       </Link>
       <Card.Body>
-      <Link to={`/product/${product._id}`}>
-        <Card.Title as='div'>
-          <strong>{product.name}</strong>
-        </Card.Title>
-      </Link>
-      <Card.Text as='div'>
-        <Rating  
-        value={product.rating}
-        text={`${product.numReviews} reviews`}/>
-      </Card.Text>
-      <Card.Text as='h3'>
-        ${product.price}
-      </Card.Text>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as='div'>
+          <Rating  
+          value={product.rating}
+          text={`${product.numReviews} reviews`}/>
+        </Card.Text>
+        <Card.Text as='h3'>
+          ${product.price}
+        </Card.Text>
       </Card.Body>
     </Card>
       
