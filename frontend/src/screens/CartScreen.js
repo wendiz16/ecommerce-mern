5f3772bcef48e5ea2289b9eace553d3c1eed83eb
Wendi Zhang <wendizhang68@gmail.com>
2023-01-04 20:18:58 -0500

    modified cartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,8 @@
 import React, {useEffect}from 'react'
 import {useDispatch,useSelector} from 'react-redux'
-import {useParams,useSearchParams} from 'react-router-dom';
-import {Row, Col, ListGroup,Image,Form, Button,Card} from 'react-bootstrap'
+import {useParams,useSearchParams,Link} from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import {Row, Col, ListGroup,Image, Button,Card, ListGroupItem} from 'react-bootstrap'
 import Message from '../components/Message'
 import {addToCart} from '../actions/cartActions'
 
@@ -11,11 +12,75 @@ const CartScreen = () => {
   const productId= params.id
   //https://stackoverflow.com/questions/73262988/react-router-v6-history-location-search-replacement
   const [searchParams, setSearchParams]= useSearchParams()
-  const qty=searchParams.get("qty")
-  console.log(qty)
-  
+  const qty=searchParams.get("qty")?Number(searchParams.get("qty")):1
+  console.log(typeof qty)
+  const dispatch =useDispatch()
+  const cart=useSelector(state=>state.cart)
+  const { cartItems } =cart
+  console.log(cartItems)
+  useEffect(()=>{
+    if(productId){
+      dispatch(addToCart(productId,qty))
+    }
+  },[dispatch,productId,qty])
+
+  const removeFromCartHandler=(id)=>{
+    console.log('remove')
+  }
   return (
-   <div>Cart</div>
+   <Row>
+     <Col md={8}>
+      <h1>Shopping Cart</h1>
+      {cartItems.length===0?<Message>Your cart is empty <Link to='/'>Go Back</Link></Message>:(
+        <ListGroup variant='flush'>
+          {cartItems.map((item)=>(
+            <ListGroup.Item key={item.product}>
+              <Row>
+                <Col md={3}>
+                  <Image src={item.image} alt={item.name} fluid rounded/>
+                </Col>
+                <Col md={3}>
+                  <Link to={`/product/${item.product}`}>{item.name}</Link>
+                </Col>
+                <Col md={2}>${item.price}</Col>
+                <Col md={2}>
+                  <Form.Control
+                   as="select"
+                   value={item.qty}
+                   onChange={(e)=>dispatch(addToCart(item.product,
+                   Number(e.target.value)))}>
+                    {
+                      [...Array(item.countInStock).keys()].map((x)=>(
+                        <option key={x+1} value={x+1}>
+                         {x+1}
+                        </option>
+                      ))
+                    }
+                  </Form.Control>
+                </Col>
+                <Col md={2}>
+                  <Button type='button' variant='light' onClick={()=>
+                    removeFromCartHandler(item.product)}>
+                    <i className='fas fa-trash'></i>
+                  </Button>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
+     </Col>
+     <Col md={4}>
+      <Card>
+        <ListGroup variant="flush">
+          <ListGroupItem>
+            <h2>Subtotal({cartItems.reduce((acc,item)=>acc+item.qty,0)}) Items</h2>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+     </Col>
+     
+   </Row>
   )
 }
 
