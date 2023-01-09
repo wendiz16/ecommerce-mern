81764edf5b8aecc2b3623d7a617c1f01b1408a6f
Wendi Zhang <wendizhang68@gmail.com>
2023-01-09 16:21:35 -0500

    added productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,29 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// Fetch all products
+// GET /api/products
+// Public fetch
+const getProducts= asyncHandler(async(req,res)=>{
+  const products = await Product.find({})
+  res.json(products);
+})
+
+
+// Fetch one product
+// GET /api/products/:id
+// Public fetch
+const getProductById= asyncHandler(async(req,res)=>{
+  const product= await Product.findById(req.params.id)
+  if(product){
+    res.json(product)
+  }else{
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export {
+  getProducts,
+  getProductById
+}
