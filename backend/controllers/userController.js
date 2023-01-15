eb02169a03957c9c28545ef426691bd0c2785a5a
Wendi Zhang <wendizhang68@gmail.com>
2023-01-14 22:44:25 -0500

    set up jwt, middleware and update controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // auth user and get token 
@@ -15,7 +16,7 @@ const authUser= asyncHandler(async(req,res)=>{
         name:user.name,
         email:user.email,
         isAdmin:user.isAdmin,
-        token:null
+        token:generateToken(user._id)
       }
     )
   }else{
@@ -24,4 +25,25 @@ const authUser= asyncHandler(async(req,res)=>{
   }
 })
 
-export {authUser}
+// Get user profile
+// GET/api/users/profile
+// Private
+const getUserProfile= asyncHandler(async(req,res)=>{
+  const user = await User.findById(req.user._id)
+  if(user){
+   res.json(
+    {
+      _id:user._id,
+      name:user.name,
+      email:user.email,
+      isAdmin:user.isAdmin,
+    }
+   )
+  }else{
+    res.status(404)
+    throw new Error('user not found')
+  }
+}
+)
+
+export {authUser,getUserProfile}
