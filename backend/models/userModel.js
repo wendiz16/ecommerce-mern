8e2bb0803028a8c41cdb3a023030cd43ff5a93e5
Wendi Zhang <wendizhang68@gmail.com>
2023-01-16 15:29:12 -0500

    enabled user registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,5 +26,13 @@ const userSchema=mongoose.Schema({
 userSchema.methods.matchPassword=async function(enteredPw){
  return await bcrypt.compare(enteredPw, this.password)
 }
+
+userSchema.pre('save',async function(next){
+  if(!this.isModified('password')){
+     next()
+  }
+ const salt= await bcrypt.genSalt(10)
+ this.password=await bcrypt.hash(this.password,salt)
+})
 const User = mongoose.model('User', userSchema);
 export default User;
