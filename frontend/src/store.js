3ae0909e84fed1a7b46df23b67f46d91bde199b5
Wendi Zhang <wendizhang68@gmail.com>
2023-01-19 12:46:04 -0500

    added userLogin reducer and action creator function and local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,8 +12,13 @@ const reducer = combineReducers({
 })
 
 const cartItemsFromStorage=localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')):[]
+const userInfoFromStorage=localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo')):null
 const initialState={
-  cart:{ cartItems:cartItemsFromStorage}
+  cart:{ cartItems:cartItemsFromStorage},
+  userLogin:{
+    userInfo:userInfoFromStorage
+  }
+  
 }
 const middleware=[thunk]
 const store=createStore(reducer, initialState,composeWithDevTools(
