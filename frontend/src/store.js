b86951660dd6b8ff65599a2657ca784790c107d3
Wendi Zhang <wendizhang68@gmail.com>
2023-01-19 21:49:46 -0500

    added logout and user name in navbar;added userRegister reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,13 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from'redux-devtools-extension'
 import {productListReducer,productDetailsReducer} from './reducers/productReducers'
 import { cartReducer } from "./reducers/cartReducers"
-import {userLoginReducer} from './reducers/userReducers'
+import {userLoginReducer, userRegisterReducer} from './reducers/userReducers'
 const reducer = combineReducers({
  productList:productListReducer,
  productDetails:productDetailsReducer,
  cart:cartReducer,
- userLogin:userLoginReducer
+ userLogin:userLoginReducer,
+ userRegister:userRegisterReducer
 })
 
 const cartItemsFromStorage=localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')):[]
@@ -17,8 +18,7 @@ const initialState={
   cart:{ cartItems:cartItemsFromStorage},
   userLogin:{
     userInfo:userInfoFromStorage
-  }
-  
+  } 
 }
 const middleware=[thunk]
 const store=createStore(reducer, initialState,composeWithDevTools(
