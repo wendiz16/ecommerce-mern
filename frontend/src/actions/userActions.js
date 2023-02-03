80b5870dbe83b224b606f9efdd535c56d29fe2d4
Wendi Zhang <wendizhang68@gmail.com>
2023-02-02 20:59:58 -0500

    added functions to userActions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,6 +1,9 @@
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS,USER_LOGOUT,
-  USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL} from "../constants/userConstants"
+  USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, 
+  USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL} from "../constants/userConstants"
 import axios from "axios"
+
+
 export const login=(email, password)=>async(dispatch)=>{
  try{
   dispatch({
@@ -62,6 +65,36 @@ export const register=(name, email, password)=>async(dispatch)=>{
   }catch(error){
    dispatch({
      type:USER_REGISTER_FAIL,
+     payload:error.response && error.response.data.message ?
+     error.response.data.message:error.message
+   })
+  }
+ }
+
+ export const getUserDetails=(id)=>async(dispatch, getState)=>{
+  try{
+   dispatch({
+    type:USER_DETAILS_REQUEST
+   })
+
+   const { userLogin:{userInfo} }=getState()
+   const config={
+     headers:{
+       "Content-Type":"application/json",
+       Authorization: `Bearer ${userInfo.token}`
+     }
+   }
+   const { data }=await axios.get(`/api/users/${id}`,
+   config)
+   
+   dispatch({
+     type:USER_DETAILS_SUCCESS,
+     payload:data
+   })
+   
+  }catch(error){
+   dispatch({
+     type:USER_DETAILS_FAIL,
      payload:error.response && error.response.data.message ?
      error.response.data.message:error.message
    })
