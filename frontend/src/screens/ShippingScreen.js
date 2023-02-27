2d9bb1286b8340948bfa349d25c728eafcbd6996
Wendi Zhang <wendizhang68@gmail.com>
2023-02-27 16:16:26 -0500

    added payment screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import{ Button } from 'react-bootstrap'
 import { useDispatch,useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 import { saveShippingAddress } from '../actions/cartActions';
 const ShippingScreen = () => {
   
@@ -22,6 +23,7 @@ const ShippingScreen = () => {
   }
   return (
     <FormContainer>
+     <CheckoutSteps step1 step2/> 
      <h1>Shipping</h1>
      <Form onSubmit={submitHandler}>
      <Form.Group controlId='address'>
